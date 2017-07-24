f8edf6699e5289ea477118429983be020982c5f5
Alexey Romanov <alexeyromanov365@gmail.com>
2017-07-24 13:02:36 +0300

    MyMiddleware moved to lib

--- app/middleware/my_middleware.rb
+++ app/middleware/my_middleware.rb
@@ -1,21 +1,21 @@
-class MyMiddleware
-  def initialize app, formatting_char = '='
-    @app = app
-    @formatting_char = formatting_char
-  end
+# class MyMiddleware
+#   def initialize app, formatting_char = '='
+#     @app = app
+#     @formatting_char = formatting_char
+#   end
 
-  def call env
-    request_started_on = Time.now
-    @status, @headers, @response = @app.call(env)
-    request_ended_on = Time.now
-    write_log("Request took #{request_ended_on - request_started_on} seconds.")
-    [@status, @headers, @response]
-  end
+#   def call env
+#     request_started_on = Time.now
+#     @status, @headers, @response = @app.call(env)
+#     request_ended_on = Time.now
+#     write_log("Request took #{request_ended_on - request_started_on} seconds.")
+#     [@status, @headers, @response]
+#   end
 
-  private
+#   private
 
-  def write_log(message)
-  	log_data = "[#{Time.now}]: #{message}\n"
-  	File.write('log/middleware.log', log_data, mode: 'a+')
-  end
-end
+#   def write_log(message)
+#   	log_data = "[#{Time.now}]: #{message}\n"
+#   	File.write('log/middleware.log', log_data, mode: 'a+')
+#   end
+# end
--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require_relative '../lib/my_middleware'
+
 module Pictagram
   class Application < Rails::Application
     config.app_generators.scaffold_controller :responders_controller
     config.load_defaults 5.1
-    config.middleware.use "MyMiddleware", "*"
+    config.middleware.use ::MyMiddleware
   end
 end
